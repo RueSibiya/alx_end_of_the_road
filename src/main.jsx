c001f7243c8e9795db05ccb93b59020173e58ea2
RueSibiya <rksibiya1@gmail.com>
2025-01-07 22:48:24 +0200

    main updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -10,4 +10,22 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx"; // Main App component
+import "./index.css"; // Global CSS styles
+
+// Import styles for the Slick carousel
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// Render the React application into the root DOM element
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* Ensures the app runs in strict mode to highlight potential issues */}
+    <App />
+  </React.StrictMode>
 );
+
