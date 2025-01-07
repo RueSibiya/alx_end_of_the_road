a451b8fa033d313eb0e8ce70fca05b0c624fa71e
RueSibiya <rksibiya1@gmail.com>
2025-01-07 22:53:24 +0200

    updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+// PostCSS configuration file
 export default {
   plugins: {
+    // Enables TailwindCSS for utility-first styling
     tailwindcss: {},
+
+    // Adds vendor prefixes to CSS rules for better cross-browser compatibility
     autoprefixer: {},
   },
-}
+};
